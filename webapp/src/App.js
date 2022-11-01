358b52c6960d942454c984f03cc9115b48819e31
Umar1311829 <star_umar2008@hotmail.com>
2022-11-01 16:57:59 +0000

    update file

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
       <h1>hello</h1>
+      <Form />
     </div>
   );
 }
---
+++ webapp/src/FormSignUp.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FormSignUp = () => {
+  return (
+    <div>
+      <h1>FormSignUp1</h1>
+    </div>
+  );
+};
+
+export default FormSignUp;
