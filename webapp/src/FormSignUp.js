85df158aa498430a5f4ffc6c76bda9a87f4ace4f
Umar1311829 <star_umar2008@hotmail.com>
2022-11-02 14:15:56 +0000

    basic structure for create user account

--- webapp/src/FormSignUp.js
+++ webapp/src/FormSignUp.js
@@ -3,7 +3,28 @@ import React from "react";
 const FormSignUp = () => {
   return (
     <div>
-      <h1>FormSignUp1</h1>
+      {/* come back to put action and method */}
+      <form>
+        <fieldset>
+          <legend>Create your account</legend>
+          <label for="username">Username</label>
+          <input type="text" name="username" id="username" placeholder="Enter your username" />
+
+          <label for="email">Email</label>
+          <input type="email" name="email" id="email" placeholder="Enter your email" />
+
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" placeholder="Enter your password" />
+
+          <label for="password2">Confirm password</label>
+          <input type="password2" name="password2" id="password2" placeholder="Enter your password" />
+
+          <button type="submit">Sign up</button>
+          <span>
+            Already have an account? Login <a href="#">here</a>
+          </span>
+        </fieldset>
+      </form>
     </div>
   );
 };
