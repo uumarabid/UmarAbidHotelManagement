99e9ad0ace1db9cf30f10880deebf85e7caa4445
Umar1311829 <star_umar2008@hotmail.com>
2022-11-03 17:47:30 +0000

    adot ui- material for styling

--- webapp/src/FormSignUp.js
+++ webapp/src/FormSignUp.js
@@ -1,63 +1,94 @@
 import React from "react";
 import useForm from "./useForm";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { Button, TextField, Container, Grid, CssBaseline } from "@mui/material";
 
 const FormSignUp = () => {
   // extract data from useForm
   const { handleChange, values, handleSubmit } = useForm();
+  const theme = createTheme();
 
   return (
-    <div>
-      {/* come back to put action and method */}
-      <form onSubmit={handleSubmit}>
-        <fieldset>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container component="main" maxWidth="xs">
+        {/* come back to put action and method */}
+
+        <form onSubmit={handleSubmit}>
           <legend>Create your account</legend>
-          <label for="username">Username</label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            placeholder="Enter your username"
-            value={values.username}
-            onChange={handleChange}
-          />
+          <Grid container rowSpacing={1}>
+            <Grid item xs={4}>
+              <label for="username">Username</label>
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="username"
+                name="username"
+                placeholder="Enter your username"
+                value={values.email}
+                onChange={handleChange}
+                label="username"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <label for="email">Email</label>
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="email"
+                name="email"
+                placeholder="email"
+                value={values.email}
+                onChange={handleChange}
+                label="Email"
+                variant="outlined"
+              />
+            </Grid>
 
-          <label for="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Enter your email"
-            value={values.email}
-            onChange={handleChange}
-          />
+            <Grid item xs={4}>
+              <label for="password">Password</label>
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="password"
+                name="password"
+                placeholder="Enter your password"
+                value={values.password}
+                onChange={handleChange}
+                label="Password"
+                variant="outlined"
+              />
+            </Grid>
 
-          <label for="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            placeholder="Enter your password"
-            value={values.password}
-            onChange={handleChange}
-          />
-
-          <label for="password2">Confirm password</label>
-          <input
-            type="password"
-            name="password2"
-            id="password2"
-            placeholder="Confirm your password"
-            value={values.password2}
-            onChange={handleChange}
-          />
-
-          <button type="submit">Sign up</button>
-          <span>
-            Already have an account? Login <a href="#">here</a>
-          </span>
-        </fieldset>
-      </form>
-    </div>
+            <Grid item xs={4}>
+              <label for="password2">Confirm password</label>
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="password2"
+                name="password2"
+                placeholder="Confirm your password"
+                value={values.password2}
+                onChange={handleChange}
+                label="Password"
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained">
+                Sign up
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <span>
+                Already have an account? Login <a href="#">here</a>
+              </span>
+            </Grid>
+          </Grid>
+        </form>
+      </Container>
+    </ThemeProvider>
   );
 };
 
--- webapp/src/validateInfo.js
+++ webapp/src/validateInfo.js
@@ -8,20 +8,21 @@ function validateInfo() {
 
   if (!values.email) {
     errors.email = "Email required";
-  }else if(!){
-    errors.email = "Email address is invalid!"
+    // complete this
+  } else if (!/^/i.test(values.email)) {
+    errors.email = "Email address is invalid!";
   }
 
-  if(!values.password){
+  if (!values.password) {
     errors.password = "Password is required";
-  }else if(values.password.length < 8){
+  } else if (values.password.length < 8) {
     errors.password = "Password needs to be 8 characters or more";
   }
 
-  if(!values.password2){
+  if (!values.password2) {
     errors.password2 = "Password is required";
-  }else if(values.password2 !== values.password){
-    errors.password2 = "Password do not match"
+  } else if (values.password2 !== values.password) {
+    errors.password2 = "Password do not match";
   }
 
   return errors;
