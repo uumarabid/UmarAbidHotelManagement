2af8b681ebca3280f90f4dad58abecdc13c9955e
Umar1311829 <star_umar2008@hotmail.com>
2023-01-24 12:13:40 +0000

    created guests table

--- Database/table.sql
+++ Database/table.sql
@@ -25,3 +25,16 @@ CREATE TABLE `hotelmanagement`.`employee` (
   `floor_number` INT NULL,
   `facilities` VARCHAR(100) NULL,
   PRIMARY KEY (`id`));
+  
+  CREATE TABLE `hotelmanagement`.`guests` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(45) NULL,
+  `last_name` VARCHAR(45) NULL,
+  `number_of_guests` INT NULL,
+  `address` VARCHAR(100) NULL,
+  `phone_number` VARCHAR(45) NULL,
+  `email` VARCHAR(60) NULL,
+  `check_in_date` DATETIME NULL,
+  `check_out_date` DATETIME NULL,
+  `is_reserved` BIT NULL,
+  PRIMARY KEY (`id`));
