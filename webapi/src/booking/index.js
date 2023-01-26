a79d0c71401d02271c31a39c11ea0785ac4e38b2
Umar1311829 <star_umar2008@hotmail.com>
2023-01-26 14:14:33 +0000

    booking routing update

--- webapi/src/booking/index.js
+++ webapi/src/booking/index.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { addBooking, deleteBokkig, editBokking, getAllBooking, getBokking } from "./controller.js";
 
-const bookingRoutes = Router()
+const bookingRoute = Router()
   .post("/add", addBooking)
   .post("/edit", editBokking)
   .get("/get", getBokking)
   .get("/getAll", getAllBooking)
   .post("/delete", deleteBokkig);
 
-export default bookingRoutes;
+export default bookingRoute;
