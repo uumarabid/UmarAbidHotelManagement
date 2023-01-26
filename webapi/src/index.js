91aa8fb425b582ed4aa38f7f2ccdd2c8f43e351f
Umar1311829 <star_umar2008@hotmail.com>
2023-01-26 13:21:38 +0000

    reservating routing

--- webapi/src/index.js
+++ webapi/src/index.js
@@ -2,8 +2,9 @@
 import { Router } from "express";
 import roomRoute from "./room/index.js";
 import userRoute from "./user/index.js";
+import reservationRoute from "./reservation/index.js";
 
 //Routing
-const routes = Router().use("/room", roomRoute).use("/user", userRoute);
+const routes = Router().use("/room", roomRoute).use("/user", userRoute).use("/reservation", reservationRoute);
 
 export default routes;
