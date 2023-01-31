e05cc3e64e93cafd03dd2e2dc1adc310e49124e5
Umar1311829 <star_umar2008@hotmail.com>
2023-01-31 13:00:45 +0000

    add packages
    - body-parser -- to receive data
    - cors -- cross origin requests

--- webapi/index.js
+++ webapi/index.js
@@ -1,10 +1,15 @@
 import routes from "./src/index.js";
 import express from "express";
 import env from "dotenv";
+import cors from "cors"; // need this package for cross origin requests, (for any request that comes from outside of the application)
+import bodyParser from "body-parser"; //  using this package to receive data in the api call, as sending paramteres to api calls like get, add, update
 
 env.config();
 
-const app = express().use(routes);
+const app = express().use(cors()).use(routes);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use(cors({ origin: true, credentials: false })); // enable cors requests for the application
 
 const port = 3001;
 app.get("/", async (req, res) => {
