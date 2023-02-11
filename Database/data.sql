9239f8c4f72a98fcfad1565408551bb7fde5c910
Umar1311829 <star_umar2008@hotmail.com>
2023-02-11 01:02:31 +0000

    udpate in sql room script

--- Database/data.sql
+++ Database/data.sql
@@ -12,33 +12,33 @@ INSERT INTO `hotelmanagement`.`users` (`user_name`, `password`, `employee_id`, `
 
 
 -- Rooms data
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('101', 'standard', '1');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('102', 'standard', '1');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('103', 'standard', '1');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('104', 'standard', '1');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('105', 'standard', '1');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('106', 'standard', '1');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('201', 'deluxe', '2');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('202', 'deluxe', '2');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('203', 'deluxe', '2');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('204', 'deluxe', '2');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('205', 'deluxe', '2');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('206', 'deluxe', '2');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('301', 'suites', '3');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('302', 'suites', '3');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('303', 'suites', '3');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('304', 'suites', '3');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('305', 'suites', '3');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('401', 'executive', '4');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('402', 'executive', '4');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('403', 'executive', '4');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('404', 'executive', '4');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('405', 'executive', '4');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('406', 'executive', '4');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('501', 'luxury', '5');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('502', 'luxury', '5');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('503', 'luxury', '5');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('504', 'luxury', '5');
-INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`) VALUES ('505', 'luxury', '5');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('101', 'standard', '1','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('102', 'standard', '1','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('103', 'standard', '1','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('104', 'standard', '1','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('105', 'standard', '1','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('106', 'standard', '1','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('201', 'deluxe', '2','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('202', 'deluxe', '2','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('203', 'deluxe', '2','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('204', 'deluxe', '2','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('205', 'deluxe', '2','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('206', 'deluxe', '2','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('301', 'suites', '3','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('302', 'suites', '3','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('303', 'suites', '3','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('304', 'suites', '3','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('305', 'suites', '3','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('401', 'executive', '4','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('402', 'executive', '4','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('403', 'executive', '4','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('404', 'executive', '4','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('405', 'executive', '4','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('406', 'executive', '4','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('501', 'luxury', '5','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('502', 'luxury', '5','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('503', 'luxury', '5','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('504', 'luxury', '5','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
+INSERT INTO `hotelmanagement`.`rooms` (`room_number`, `room_type`, `floor_number`, `facilities`) VALUES ('505', 'luxury', '5','Fridge, Dinning table with chairs, Locker, TV, Hair dryer');
 
 
