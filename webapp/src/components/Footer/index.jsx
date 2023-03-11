9ca63e742adcf60f81c1a5781f02499b3e06bef0
Umar1311829 <star_umar2008@hotmail.com>
2023-03-11 15:37:15 +0000

    Add styling

--- webapp/src/components/Footer/index.jsx
+++ webapp/src/components/Footer/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Box, IconButton } from "@mui/material";
 
 const Footer = (props) => {
   return (
     <footer>
       <p>Copyright {props.year}</p>
+      {/* <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}> */}
+      <InstagramIcon />
+      <FacebookIcon />
+      <TwitterIcon />
+      <LinkedInIcon />
+      {/* </IconButton> */}
     </footer>
   );
 };
